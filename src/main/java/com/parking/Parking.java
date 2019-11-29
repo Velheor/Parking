1260a9b797abe4fda32f5f9970e455b2ec96fe53
“Vlad <vlad.atsman@gmail.com>
2019-11-29 13:36:23 +0300

    Initial commit

---
+++ src/main/java/com/parking/Parking.java
@@ -0,0 +1,47 @@
+package com.parking;
+
+import com.parking.threading.Car;
+import com.parking.threading.Spot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class Parking {
+    static List<Spot> spots = new ArrayList<Spot>();
+    static BlockingQueue<Car> carQueue = new ArrayBlockingQueue<Car>(10, true);
+
+    public static void main(String[] args) throws InterruptedException {
+        Parking parking = new Parking();
+
+        for (int i = 1; i <= 2; i++) {
+            parking.getSpots().add(new Spot(i, parking));
+        }
+        //parking.getPlaces().forEach(Thread::start);
+
+        for (Spot spot : parking.getSpots()) {
+            spot.start();
+        }
+        Thread.sleep(100);
+
+        for (int i = 1; i <= 2; i++) {
+            new Car(i, parking).start();
+        }
+
+        Thread.sleep(100);
+
+        for (int i = 3; i <= 5; i++) {
+            new Car(i, parking).start();
+        }
+
+    }
+
+    public List<Spot> getSpots() {
+        return spots;
+    }
+
+    public BlockingQueue<Car> carQueue() {
+        return carQueue;
+    }
+}
